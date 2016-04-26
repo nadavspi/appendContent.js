218030d7abfcc8125b7a8f9c88d973115093dd80
Nadav Spiegelman <nspiegelman@somethingdigital.com>
2016-04-26 16:58:24 -0400

    Clean up comment

--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@
   /**
    * Appends some content to an element and adds a class.
    *
-   * We use a CSS pseudo element to append "required" or "optional" to input lrabels.
-   * The problem is that some screen readers don't announce generated content.
-   * To get around this, we're going to inject the content to the label into the DOM and hide the pseudo class content.
-   *
    * @param {Object} options
    * @param {string} options.className - Class name to apply once the content is appended
    * @param {string} options.content - Content to append to the element
